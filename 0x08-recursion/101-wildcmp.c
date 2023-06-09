decfc7b78479a821b7bea6733d4a736d9b9482e0
Nglyase <ngozokoye@gmail.com>
2023-06-09 08:41:17 +0000

    don't say it is impossible

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,12 +11,12 @@ int wildcmp(char *s1, char *s2)
 	if (*s1 == '\0' && *s2 == '\0')
 		return (1);
 	if (*s2 == *s1)
-		return (wildcmp(s2 + 1, s1 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s2 == '*')
 	{
 		if (wildcmp(s1, s2 + 1))
 			return (1);
-		if (*s2 != '\0' && wildcmp(s1 + 1, s2))
+		if (*s1 != '\0' && wildcmp(s1 + 1, s2))
 			return (1);
 	}
 	return (0);
