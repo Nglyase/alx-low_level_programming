6e72fa1a71fc656592bb6ecc03fbf07ca8c20565
Nglyase <ngozokoye@gmail.com>
2023-06-12 08:28:16 -0700

    modifies main

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,11 @@
 #include "main.h"
 
+/**
+ * main - Entry point
+ * Return: 0
+ */
 int main(void)
 {
-	_puts("\"At the end of day, my goal was to be the best"- Kevin Mitnick");
+	_puts("\"At the end of day, my goal was to be the best" - Kevin Mitnick");
 	return (0);
 }
