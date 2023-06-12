81ee62e98a1a459002f7f4944c9be15e85cb49d3
Nglyase <ngozokoye@gmail.com>
2023-06-12 14:06:00 +0000

    Everyone needs a Library

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,17 +1,14 @@
 #include "main.h"
 /**
- *_isupper - checks if a character is upper case or not
- *@c: the character
- *Return: 0
+ * _isupper - uppercase letters
+ * @c: char to check
+ *
+ * Return: 0 or 1
  */
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,2 +1,5 @@
 A task on | Static Liabraries
 A collection of my fuctions in C created so far.
+            TASK
+0. A library is not a luxury but one of the necessities of life
+1. Without libraries what have we? We have no past and no future
