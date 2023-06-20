e9795ef44c6ab6894f59b88835c25e90736317f7
Nglyase <ngozokoye@gmail.com>
2023-06-20 03:22:38 +0000

    Define dog

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+0x0E-structures_typedef
+This is a C project on strucures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,28 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - a dog structure
+ *@name: the name of the dog
+ *@age: the age of the dog
+ *@owner: the owner of the dog
+ *
+ *description: a structure for a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+
+
+#endif /* DOG_H */
