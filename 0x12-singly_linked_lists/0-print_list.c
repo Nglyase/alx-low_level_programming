90e8bad509ab929df6db6ae8509775b8a81c9841
Nglyase <ngozokoye@gmail.com>
2023-07-04 23:14:39 +0000

    Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a given list
+ *@h: the list
+ *
+ *Return: number of elements
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t numElem;
+
+	numElem = 0;
+
+	for (numElem = 0; h != NULL; numElem++)
+	{
+		if (h->str == NULL)
+			printf("%d %s\n", 0, "(nil)");
+		else
+			printf("%d %s\n", h->len, h->str);
+		h = h->next;
+	}
+	return (numElem);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,4 @@
+0x12. C - Singly linked lists
+A project on singly linked lists in C.
+Understanding of what lists are, how to build them
+and how to use them.
